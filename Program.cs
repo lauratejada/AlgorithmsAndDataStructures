14e7cb233974c238119b00a4f34f7c27e99160e1
LauraTejada <laura2191@hotmail.com>
2023-06-06 20:54:54 -0500

    First commit lab04

--- Program.cs
+++ Program.cs
@@ -1,147 +1,112 @@
-﻿/************ LAB 03 **************/
-/*Lab - Complexity
-Create new methods to solve each of the following problems.
+﻿/************ LAB 4 - LISTS **********/
+/*
+ * 1. List<int> MaxNumbersInLists(List<List<int>>) 
+ * accepts as a parameter a List of Lists of Integers. 
+ * It returns a new list with each element representing 
+ * the maximum number found in the corresponding original list. 
+ * For example, { {1, 5, 3}, {9, 7, 3, -2}, {2, 1, 2} } returns {5, 9, 2}. 
+ * Output the results with a message like: “List 1 has a maximum of 5. 
+ * List 2 has a maximum of 9. List 3 has a maximum of 2.”
+ */
+using System.Collections.Generic;
 
-1. We have a list of integers where elements appear either once or twice. 
-Find the elements that appeared twice in O(n) time.
-example: {1, 2, 3, 4, 7, 9, 2, 4} returns '{2, 4}
-The solution time complexity is: O3n or On
-*/
 Console.WriteLine("Instruction 1:");
 
-int[] input = { 1, 2, 3, 4, 7, 9, 2, 4 };
+List<List<int>> input = new List<List<int>> { new List<int>() { 1, 5, 3 }, new List<int>() { 9, 7, 3, -2 }, new List<int>() { 2, 1, 2 } };
 
-HashSet<int> uniqueValues = new HashSet<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 };
-
-Dictionary<int, int> intCounter = new Dictionary<int, int>();
-
-// if the give input has content of integers
-if (input.Length > 0)
+// Create the method to get the highest values of each subList
+static List<int> maxNumbersInLists(List<List<int>> list)
 {
-    Console.WriteLine("Given array is: ");
-    Console.WriteLine(string.Join(", ", input));
-    //Console.WriteLine(String.Join(", ", uniqueValues.ToArray()));
-
-    // first, we fill our dictionary with unique integers [0 -9];
-    foreach (int integer in uniqueValues)
+    List<int> listTemp = new List<int>();
+    foreach (List<int> collection in list)
     {
-        intCounter.Add(integer, 0);
+        if (collection.Count > 0)
+        {
+            listTemp.Add(collection.Max());
+        }
     }
-    // second, we fill our dictionary with the number of integers in the given array;
-    foreach (int integer in input)
-    {
-        intCounter[integer]++;
-    }
-    //  third, print the integer that appear twice;
-    Console.WriteLine("The result is:");
-    foreach (KeyValuePair<int, int> element in intCounter)
-    {
-        if (element.Value == 2)
-            Console.Write($"{element.Key}, ");
-    }
-
-}
-else
-{
-    Console.WriteLine("The input should be an array of integers");
+    return listTemp;
 }
 
-Console.WriteLine();
+// Validate length of List
+if (input.Count() <= 0)
+{
+    throw new Exception("The datatype of the inner list has to be integers");
+}
+
+List<int> finalList = maxNumbersInLists(input);
+// Printing the final results
+foreach (int item in finalList)
+{
+    Console.Write($"List {finalList.IndexOf(item) + 1} has a maximum of ");
+    Console.Write($"{item}.");
+    Console.WriteLine();
+}
+
+Console.WriteLine("The time complexity of this solution is O2n or O(n)");
 Console.WriteLine("-------------------");
+Console.WriteLine();
 
 /*
-2. We have two sorted int arrays which could be with different sizes. 
-We need to merge them in a third array while keeping this result array sorted. 
-Can you do that in O(n) time? 
-Don't use any extra structures like Sets or Dictionaries
-example: {{1, 2, 3, 4, 5}, {2, 5, 7, 9, 13}}
-returns {1, 2, 2, 3, 4, 5, 5, 7, 9, 13}
-*/
-
+ * 2. String HighestGrade(List<List<int>>) 
+ * accepts a list of number grades among students in different courses 
+ * (where each list represents a single course), between 0 and 100. 
+ * It returns the highest grade among all students in all courses.
+ * For example: { {85,92, 67, 94, 94}, {50, 60, 57, 95}, {95} } 
+ * returns "The highest grade is 95. 
+ * This grade was found in class(es) {index}".
+ */
 Console.WriteLine("Instruction 2:");
 
-int[] intArray1 = new int[] { 1, 2, 3, 4, 5 };
-int[] intArray2 = new int[] { 2, 5, 7, 9, 13 };
+List<List<int>> grades = new List<List<int>> { new List<int>() { 85, 92, 67, 94, 94 }, new List<int>() { 50, 60, 57, 95 }, new List<int>() { 95 } };
 
-if (intArray1.Length <= 0 && intArray2.Length <= 0)
+// validate list of grades
+if (grades.Count() <= 0)
 {
-    throw new Exception("The input is not valid, please enter two arrays of integers");
+    throw new Exception("The datatype of the inner list has to be integers");
 }
 
-Console.WriteLine("Given arrays are:");
-Console.WriteLine(string.Join(", ", intArray1));
-Console.WriteLine(string.Join(", ", intArray2));
+List<int> highestGrades = maxNumbersInLists(grades);
 
-int[] intArray3 = new int[intArray1.Length + intArray2.Length];
-int index1 = 0;
-int index2 = 0;
-int index3 = 0;
-// Compare and assign the values of each array into the new array
-// until the lower array length is reached
-while (index1 < intArray1.Length && index2 < intArray2.Length)
-{
-    if (intArray1[index1] < intArray2[index2])
-    {
-        intArray3[index3] = intArray1[index1];
-        index1++;
-        index3++;
-    }
-    else if (intArray1[index1] >= intArray2[index2])
-    {
-        intArray3[index3] = intArray2[index2];
-        index2++;
-        index3++;
-    }
-}
-
-//Console.WriteLine("First while");
-// If the length of this array is higher than the other array(intArray1.Length > intArray2.Length),
-// so save the rest of its values in the new array
-while (index1 < intArray1.Length)
-{
-    intArray3[index3] = intArray1[index1];
-    index1++;
-    index3++;
-}
-//Console.WriteLine("Second while");
-// If the length of this array is higher than the other array(intArray2.Length > intArray1.Length),
-// so save the rest of its values in the new array
-while (index2 < intArray2.Length)
-{
-    intArray3[index3] = intArray2[index2];
-    index2++;
-    index3++;
-}
-//Console.WriteLine("Third while");
-Console.WriteLine("The result is: ");
-Console.Write(string.Join(", ", intArray3));
-//Console.Write("} ");
-
+Console.WriteLine($"The highest grade is {highestGrades.Max()}");
+Console.WriteLine($"This grade was found in grade {highestGrades.IndexOf(highestGrades.Max()) + 1}");
+Console.WriteLine("The time complexity of this solution is O1n or O(n)");
 Console.WriteLine();
-Console.WriteLine("This solution has a time complexity of O3n or O(n)");
 Console.WriteLine("-------------------");
+Console.WriteLine();
+
 /*
-3. Given an integer, reverse the digits of that integer, 
-e. g. input is 3415, output is 5143. 
-What is the time complexity of your solution?
+ * 3. List<int> OrderByLooping (List<int>) orders a list of integers, 
+ * from least to greatest, using only basic control statements (ie. if/else, for/while).
+ * For example, argument {6, -2, 5, 3} returns {-2, 3, 5, 6}.
  */
 Console.WriteLine("Instruction 3:");
-int input3 = 3415;
-if (input3 <= 0)
-{
-    throw new Exception("The input is not an valid integer");
-}
 
-Console.WriteLine("Given input is: ");
-Console.WriteLine($"{input3}");
-// convert integer into an array of chars
-char[] temp3 = input3.ToString().ToCharArray();
-Console.WriteLine("The result is: ");
-for (int i = temp3.Length - 1; i >= 0; i--)
+List<int> input3 = new List<int>() { 6, -2, 5, 3 };
+static List<int> orderByLooping(List<int> list)
 {
-    //printing each value reversed
-    Console.Write(temp3[i]);
+    int intTemp = 0;
+    /* SortedList<int, int> numberNames = new SortedList<int, int>(); */
+    for (int i = 0; i < list.Count(); i++)
+    {
+        for (int j = 0; j < list.Count(); j++)
+        {
+            if (list[i] < list[j])
+            {
+                intTemp = list[i];
+                list[i] = list[j];
+                list[j] = intTemp;
+            }
+        }
+    }
+    return list;
 }
-Console.WriteLine("");
-Console.WriteLine("The time complexity is On");
+if (input3.Count() <= 0)
+{
+    throw new Exception("The datatype of the inner list has to be integers");
+}
+List<int> orderedList = orderByLooping(input3);
+Console.WriteLine(string.Join(", ", orderedList));
+Console.WriteLine("The time complexity of this solution is On2 or O(n)2");
+Console.WriteLine();
 Console.WriteLine("-------------------");
